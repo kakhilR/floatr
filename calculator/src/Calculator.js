45919a148abf1b68366b7b05ef1388fc2fc7e11f
karenaeAkhil <akhilr@surelyhq.com>
2021-10-13 21:03:01 +0530

    added toggle effect to show result

--- calculator/src/Calculator.js
+++ calculator/src/Calculator.js
@@ -3,9 +3,18 @@ import Container from "@material-ui/core/Container"
 import Grid from "@material-ui/core/Grid"
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import { styled } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
 
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
 
 const Calculator = ()=> {
+    const [ toggle, setToggle] = useState(false);
     const [amount,setAmount] = useState()
     const [months,setMonths] = useState()
     const [interest,setInterest] = useState()
@@ -24,9 +33,11 @@ const Calculator = ()=> {
         setTotal(totalAmount)  
         let totalPayableInterest = parseInt(totalAmount)-parseInt(amount)  
         setPayableInterest(totalPayableInterest)
+        setToggle(true)
 
     }
     // console.log(payableInterest)
+    
     return (
         <Container maxWidth="sm" >
              <h2 style={{marginTop:"10px",fontSize:"25px",fontWeight:"bold",marginLeft:'100px'}}>Loan EMI Calculator</h2>
@@ -39,16 +50,16 @@ const Calculator = ()=> {
                     <TextField label="Interest%" style={{marginBottom:'20px', marginLeft:'100px'}} variant="outlined" value={interest} onChange={(e)=> setInterest(e.target.value)} />
                 </form>
                 <Button variant ="contained" color="primary" onClick = {handleSubmit}>Calculate</Button>
-                 
             </Grid>
-            <div style={{width:"500px",height:"100px",marginTop:"20px",display:"flex",justifyContent:"space-between",marginLeft:'-35px'}} className="result">
-                        <div style={{height:"50px",width:"150px", marginTop:"5px",backgroundColor:"#ffa726",borderRadius:"5px",textAlign:"center",fontFamily:'inter',color:"white"}}>{emi}</div>
-                        <div style={{height:"50px",width:"150px", marginTop:"5px",backgroundColor:"#ffa726",borderRadius:"5px",textAlign:"center",fontFamily:'inter',color:"white"}}>{total}</div>
-
-                 </div>
-                 <div style={{width:"300px",height:"100px",marginTop:"20px",justifyContent:"space-between",marginLeft:'140px'}} className="result">
-                        <div style={{height:"50px",width:"150px", marginTop:"5px",backgroundColor:"#ffa726",borderRadius:"5px",textAlign:"center",fontFamily:'inter',color:"white"}}>{payableInterest}</div>
-                 </div>
+            { (toggle === true ) ?   <div>
+                <div style={{width:"500px",height:"100px",marginTop:"20px",display:"flex",justifyContent:"space-between",marginLeft:'-35px'}} className="result">
+                    <div style={{height:"50px",width:"150px", marginTop:"5px",backgroundColor:"#ffa726",borderRadius:"5px",textAlign:"center",fontFamily:'inter',color:"white"}}>{emi}</div>
+                    <div style={{height:"50px",width:"150px", marginTop:"5px",backgroundColor:"#80d8ff",borderRadius:"5px",textAlign:"center",fontFamily:'inter',color:"white"}}>{total}</div>
+                </div>
+                <div style={{width:"300px",height:"100px",marginTop:"20px",justifyContent:"space-between",marginLeft:'140px'}} className="result">
+                    <div style={{height:"50px",width:"150px", marginTop:"5px",backgroundColor:"#a1887f",borderRadius:"5px",textAlign:"center",fontFamily:'inter',color:"white"}}>{payableInterest}</div>
+                </div>
+            </div> : <> </>}
         </Container>
     )
 }
